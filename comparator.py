c2aebe297e377fb60ec1c981d5903f36c566ee6a
princeyadav96@gmail.com <princeyadav96@gmail.com>
2017-06-30 00:21:19 +0530

    One more Loop Hole.

--- comparator.py
+++ comparator.py
@@ -16,16 +16,17 @@ x = len(data1)
 
 
 def compare():
-    for i in range(0, x):
-        if data1[i][0] == data[i][0] and data1[i][1] == data[i][1] and data1[i][2] == data1[i][2]:
-            print "For " + data1[i][0] + " and repo : " + data1[i][1] + " with " + data[i][2] + " changes are > "
-            compare_commits = int(data1[i][4]) - int(data[i][4])
-            print "Commit(s) updated = " + str(compare_commits)
-            compare_lines_added = int(data1[i][5]) - int(data[i][5])
-            print "Update on line(s) addition = " + str(compare_lines_added)
-            compare_lines_deleted = int(data[i][6]) - int(data[i][6])
-            print "Update on line(s) deletion = " + str(compare_lines_deleted)
-        else:
-            print "Nothing to compare for " + data1[i][0] + " with repo : " + data1[i][1] + " in new file !"
+    for j in range(0, x):
+        for i in range(0, x):
+            if data1[j][0] == data[i][0] and data1[j][1] == data[i][1] and data1[j][2] == data[i][2]:
+                print "For " + data1[i][0] + " and repo : " + data1[i][1] + " with " + data[i][2] + " changes are > "
+                compare_commits = int(data1[j][4]) - int(data[i][4])
+                print "Commit(s) updated = " + str(compare_commits)
+                compare_lines_added = int(data1[j][5]) - int(data[i][5])
+                print "Update on line(s) addition = " + str(compare_lines_added)
+                compare_lines_deleted = int(data[j][6]) - int(data[i][6])
+                print "Update on line(s) deletion = " + str(compare_lines_deleted)
+            else:
+                print "Nothing to compare for " + data1[i][0] + " with repo : " + data1[i][1] + " in new file !"
 
 compare()
